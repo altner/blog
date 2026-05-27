efe733d1de88e6bf18d5b68e1491e9a96b88c8a1
Adrian Altner <altner@mailbox.org>
2026-05-27 15:14:22 +0200

    Refactor blog post links to use BASE_URL directly for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,13 @@ import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://altner.github.io/blog",
+  site: "https://altner.github.io/blog/",
   base: "/blog",
   devToolbar: {
     enabled: false,
   },
   integrations: [sitemap()],
+  trailingSlash: "always",
   prefetch: true,
   vite: {
     ssr: {
